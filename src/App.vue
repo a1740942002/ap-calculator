a7163f0d7eb69b4b6389704aaf8e7a40645e8daa
a1740942002 <a1740942002@me.com>
2021-09-13 00:43:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>ATM 殺星計算機</h1>
+  <form action="">
+    <label for="">目前的深淵點：</label>
+    <input type="number" />
+    <label for="">預計達成的深淵點：</label>
+    <input type="number" />
+  </form>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>

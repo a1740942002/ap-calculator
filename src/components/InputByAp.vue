9a7c66e14f41bb1e4e6851745fb23e4c6868f87b
a1740942002 <a1740942002@me.com>
2021-09-18 07:26:46 +0800

    v6

--- src/components/InputByAp.vue
+++ src/components/InputByAp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-2">
-    <label for="">天族深淵點</label>
+    <label for="">魔族深淵點</label>
     <n-input-number
       v-model:value="userAp"
       clearable
@@ -8,23 +8,23 @@
     />
     <div v-if="userAp">
       <p v-if="userPosition.name != '總司令'">
-        天族目前為 {{ userPosition.name }}，距離下一階級 (
+        魔族目前為 {{ userPosition.name }}，距離下一階級 (
         {{ nextUserPosition.name }} ) 還差
         {{ nextUserPosition.levelUpAp - userAp }} 點。
       </p>
-      <p v-else>天族目前為 {{ userPosition.name }}，已經達到最高階級。</p>
+      <p v-else>魔族目前為 {{ userPosition.name }}，已經達到最高階級。</p>
     </div>
   </div>
 
   <div class="py-2">
-    <label for="">魔族深淵點</label>
+    <label for="">天族深淵點</label>
     <n-input-number
       v-model:value="atmAp"
       clearable
       placeholder="請輸入您目前的深淵點..."
     />
     <p v-if="atmAp">
-      魔族目前為 {{ atmPosition.name }}，掉回上一階級還差
+      天族目前為 {{ atmPosition.name }}，掉回上一階級還差
       {{ atmAp - atmPosition.levelUpAp }}
     </p>
   </div>

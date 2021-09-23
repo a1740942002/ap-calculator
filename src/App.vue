993f733775314d65ea947cb67716ccba3b809d20
a1740942002 <a1740942002@me.com>
2021-09-24 04:42:21 +0800

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,3 @@
 <template>
-  <div class="w-10/12 mx-auto space-y-8 py-8">
-    <div>
-      <h1 class="text-3xl font-semibold">深淵點互刷計算機</h1>
-      <n-divider />
-      <p>根之島很危險，算完趕快刷不要逗留。</p>
-    </div>
-
-    <div class="md:flex md:justify-between md:space-x-4">
-      <div class="flex-1">
-        <InputSection />
-      </div>
-
-      <div class="flex-1">
-        <BrushSection />
-      </div>
-    </div>
-
-    <div class="space-y-8">
-      <PositionTable />
-      <AbyssGearTable />
-    </div>
-    <n-back-top :right="32" :visibility-height="300" />
-  </div>
+  <router-view></router-view>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,25 @@
 <template>
-  <h1>Home</h1>
+  <div class="w-10/12 mx-auto space-y-8 py-8">
+    <div>
+      <h1 class="text-3xl font-semibold">深淵點互刷計算機</h1>
+      <n-divider />
+      <p>根之島很危險，算完趕快刷不要逗留。</p>
+    </div>
+
+    <div class="md:flex md:justify-between md:space-x-4">
+      <div class="flex-1">
+        <InputSection />
+      </div>
+
+      <div class="flex-1">
+        <BrushSection />
+      </div>
+    </div>
+
+    <div class="space-y-8">
+      <PositionTable />
+      <AbyssGearTable />
+    </div>
+    <n-back-top :right="32" :visibility-height="300" />
+  </div>
 </template>
-
-<script>
-import { provide, ref, reactive } from "vue";
-export default {
-  setup() {
-    const location = ref("North Pole");
-    const geolocation = reactive({
-      longitude: 93,
-      latitude: 135,
-    });
-
-    provide("location", location);
-    provide("geolocation", geolocation);
-  },
-};
-</script>
